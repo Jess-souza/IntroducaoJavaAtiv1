9b30a3dd43358c3b45a4afb226fca17adb7174c9
Jessica <jsouzasouza120@gmail.com>
2022-09-21 22:36:48 -0300

    Realizado exercício 3

--- src/exercicio3/Conversor.java
+++ src/exercicio3/Conversor.java
@@ -1,4 +1,13 @@
 package exercicio3;
 
 public class Conversor {
+
+    public static void main(String[] args) {
+        final double fator2 = 32;
+        final double fator1 = 9.0 / 5.0;
+        double fahrenheit;
+        double celsius = 64;
+        fahrenheit = (celsius * fator1) + fator2;
+        System.out.println("A conversão de " + celsius + " graus Celsius em Fahrenheit é: " + fahrenheit);
+    }
 }
