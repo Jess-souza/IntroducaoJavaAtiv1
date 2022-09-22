dd357f6c6d5cc2e595532ef740d6cda35e9736fe
Jessica <jsouzasouza120@gmail.com>
2022-09-21 22:45:37 -0300

    Realizado exercício 4

--- src/exercicio4/ConversorMoeda.java
+++ src/exercicio4/ConversorMoeda.java
@@ -1,4 +1,11 @@
 package exercicio4;
 
 public class ConversorMoeda {
+    public static void main(String[] args) {
+        double dolar = 5.17;
+        double real = 532.50;
+        double conversao = real/dolar;
+
+        System.out.printf("A conversão de R$%.2f em dólar, resulta em $%.2f", real, conversao);
+    }
 }
