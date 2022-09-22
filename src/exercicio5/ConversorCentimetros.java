0ec35253ed98d0f4e2debd3ff51f79d43d3e0ca2
Jessica <jsouzasouza120@gmail.com>
2022-09-21 22:59:26 -0300

    Realizado exercício 5

--- src/exercicio5/ConversorCentimetros.java
+++ src/exercicio5/ConversorCentimetros.java
@@ -1,4 +1,10 @@
 package exercicio5;
 
 public class ConversorCentimetros {
+    public static void main(String[] args) {
+        double polegadas = 25;
+        double cm = polegadas * 2.54;
+
+        System.out.printf("A conversão de %.0f polegadas em centimetros, resulta em %.1fcm", polegadas, cm);
+    }
 }
